700cda6c5b04ed3530c80ac123b3051a0cf3b0bd
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-08-17 16:23:58 +0700

    Fix: provide 2 options [1 word or each word]

--- api/helper/format.js
+++ api/helper/format.js
@@ -1,16 +1,23 @@
 
 
 module.exports = {
-    toCapitalize: (string = null) => {
+    toCapitalize: (string = null, applyToEach = false) => {
         if (!string) throw new Error('{toCapitalize}: Can not invoke with null paramter')
 
-        const rawWords = string.split(" ")
-        const capitalizedWords = Array()
-        rawWords.forEach(element => {
-            element = element.charAt(0).toUpperCase() + element.slice(1,element.length)
-            capitalizedWords.push(element)
-        })
-        
-        return capitalizedWords.join(" ")
-    }
+        if (applyToEach) {
+            // Capitalize each word
+            const rawWords = string.split(" ")
+            const capitalizedWords = Array()
+            rawWords.forEach(element => {
+                element = element.charAt(0).toUpperCase() + element.slice(1,element.length)
+                capitalizedWords.push(element)
+            })
+            
+            return capitalizedWords.join(" ")
+        }else {
+            // Capitalize only the first word
+            return string.charAt(0).toUpperCase() + string.slice(1, string.length)
+        }
+    },
+
 }

c1b0e7f6bbfe453e8469bbaf8fae275dbeba8165
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-10-29 17:49:05 +0700

    Fix: attach address to profile route

--- api/routes/admin/profile.route.js
+++ api/routes/admin/profile.route.js
@@ -2,17 +2,21 @@ const express = require('express')
 const router = express.Router()
 const { getProfile, updateProfile, changePasswordByUserId } = require('../../controllers/profile.controller')
 const {_getAvatar } = require('../../helper/file')
+const addressRouter = require('./address.route')
 
 // @desc:   get profile by accessTK
 // @route:  GET /profile
 router.get('/', getProfile)
 
-// @desc:   update profile by accessTK
+// @desc:   update profile by accessTK 
 // @route:  PUT /profile
 router.put('/', _getAvatar, updateProfile)
 
 // @desc:   change password by accessTK
 // @route:  PUT /change-password
 router.put('/change-password', changePasswordByUserId)
+
+/*  === Address routes === */ 
+router.use('/address', addressRouter)
 
 module.exports = router

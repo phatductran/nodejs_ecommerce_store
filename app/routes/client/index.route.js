72ecc45291799396060b3fd3943e491861a33919
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-12-03 00:40:48 +0700

    Sort contacts and add faq page

--- app/routes/client/index.route.js
+++ app/routes/client/index.route.js
@@ -1,22 +1,30 @@
 const router = require("express").Router()
 const {
-    showIndexPage, showContactPage, showAboutPage, contact
+  showIndexPage,
+  showContactPage,
+  showAboutPage,
+  contact,
+  showFAQsPage,
 } = require("../../controllers/client/index.controller")
 
 // @desc:   show index page
 // @route:  GET ['/','/index','/home']
-router.get(['/','/index','/home'], showIndexPage)
+router.get(["/", "/index", "/home"], showIndexPage)
 
 // @desc:   show index page
 // @route:  GET /about
-router.get('/about', showAboutPage)
+router.get("/about", showAboutPage)
 
 // @desc:   show contact page
 // @route:  GET /contact
-router.get('/contact', showContactPage)
+router.get("/contact", showContactPage)
 
 // @desc:   leave messages
-// @route:  GET /contact
-router.post('/contact', contact)
+// @route:  POST /contact
+router.post("/contact", contact)
+
+// @desc:   FAQ
+// @route:  GET /faqs
+router.get("/faqs", showFAQsPage)
 
 module.exports = router
